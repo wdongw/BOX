8ed01795c4f42dcf13f3b2ba6bf0e201a9df99a9
Wei Dong <w_dong_w@163.com>
2023-10-21 20:34:53 +0800

    Remove the wrong include statement, even don't know the reason of failure.

--- app/build/generated/source/buildConfig/debug/com/github/tvbox/osc/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/github/tvbox/osc/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.github.tvbox.osc.tk";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0.20231017_1417";
+  public static final String VERSION_NAME = "1.0.20231018_2118";
 }
